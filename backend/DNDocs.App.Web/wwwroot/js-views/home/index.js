db4627b3bf88e259edd2aa6b6f715b452af29073
neuroxiq <neuroxiq@wp.pl>
2026-03-21 12:45:35 +0100

    stepper

--- backend/DNDocs.App.Web/wwwroot/js-views/home/index.js
+++ backend/DNDocs.App.Web/wwwroot/js-views/home/index.js
@@ -1,11 +1,70 @@
-﻿var homeIndex = function () {
+﻿
+
+
+var homeIndex = function () {
+    var Stepper = function (element) {
+        let container = element;
+        let steps = container.children;
+        let inProgressTimeout = null;
+
+        function resetStep(index) {
+            steps[index].classList.remove('is-success');
+            steps[index].classList.remove('is-danger');
+            steps[index].classList.remove('is-info');
+            clearTimeout(inProgressTimeout);
+        }
+        
+        function animationInProgress(stepElement, i) {
+            
+            let dotsDiv = stepElement.querySelector('.dots');
+
+            if (!dotsDiv) {
+                dotsDiv = document.createElement('span');
+                dotsDiv.classList.add('dots');
+                stepElement.appendChild(dotsDiv);
+            }
+
+            dotsDiv.innerHTML = ' ' + '.'.repeat((i % 4));
+             inProgressTimeout = setTimeout(() => { console.log('a' + i); animationInProgress(stepElement, i + 1) }, 500);
+        }
+
+        function setStep(arg) {
+            let className = null;
+
+            resetStep(arg.index);
+
+            if (arg.status == 'success') {
+                className = 'is-success';
+            } else if (arg.status == 'error') {
+                className = 'is-danger';
+            } else if (arg.status == 'info') {
+                className = 'is-info';
+            } else if (arg.status == 'inprogress') {
+                className = 'is-info';
+                animationInProgress(steps[arg.index], 0);
+            }
+            
+            container.children[arg.index].classList.add(className);
+        }
+
+        return {
+            setStep: setStep
+        }
+    }
+
     let form = document.getElementById("form");
     form.addEventListener("submit", onSubmit);
+    
+    let stepper = new Stepper(document.getElementById('progress-stepper'));
+
+    //stepper.setStep({ index: 0, status: 'error' });
+    //stepper.setStep({ index: 1, status: 'success' });
+    stepper.setStep({ index: 2, status: 'inprogress' });
 
     function onSubmit(e) {
         e.preventDefault();
-
         let formData = Object.fromEntries(new FormData(form));
+       
 
         fetch("/api/Integration/NugetCreateProject", {
             method: "POST",
