1beb2c7f13c19484dc686e4a5d4b7dcbe8b16bef
neuroxiq <neuroxiq@wp.pl>
2026-03-29 17:43:19 +0200

    settings & prod tests

---
+++ backend/DNDocs.Production.Tests/Shared/ProductionTestsSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DNDocs.Production.Tests.Shared
+{
+    public class ProductionTestsSettings
+    {
+        public string DNDocsServerUrl { get;set; }
+        public string DNDocsDocsServerUrl { get;set; }
+        public string DNDocsJobServerUrl { get;set; }
+    }
+}
