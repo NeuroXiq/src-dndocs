0f8a6b133fa4066c1527df3d155378ce57fd75cd
neuroxiq <neuroxiq@wp.pl>
2026-04-06 09:34:32 +0200

    prod tests

--- build/run-tests-production.ps1
+++ build/run-tests-production.ps1
@@ -8,5 +8,5 @@ set-location "$PSScriptRoot";
 $date = (get-date).tostring("yyyyMMdd_HHmmss")
 $prodTestDir = resolve-path "../backend/DNDocs.ProductionTests";
 set-content "$prodTestDir/settings.json" -encoding utf8 -value (get-content "$prodTestDir/settings.$environment.json" -raw)
-dotnet test "$prodTestDir/DNDocs.ProductionTests.csproj" --results-directory "$PSScriptRoot/temp/tests-prod-logs-$date" --output "$PSScriptRoot/temp/tests-prod-buildout-$date";
+dotnet test "$prodTestDir/DNDocs.ProductionTests.csproj" --results-directory "$PSScriptRoot/temp/tests-prod-logs-$date" 
 set-content "$prodTestDir/settings.json" -encoding utf8 -value '{ }';
